972d83630bccdb2bd7a86956d2224302570294cd
Natacha Cerpa <natachacerpa18@gmail.com>
2026-01-15 14:48:48 -0300

    Intentando crear páginas

--- proyecto_masajista/urls.py
+++ proyecto_masajista/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 # URLs principales del proyecto
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('web.urls')),
-    path('pages/', include('pages.urls')),   
+    path('pages/', include('pages.urls')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),   
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
