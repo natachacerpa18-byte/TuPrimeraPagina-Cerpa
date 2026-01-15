b3bbdbf149a5a6ef1dfc9d4457796622027a4730
Natacha Cerpa <natachacerpa18@gmail.com>
2026-01-15 15:10:00 -0300

    Pages dropdown funcionando + detalle

--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
-from .views import (
-    PageListView, PageDetailView, PageCreateView, PageUpdateView, PageDeleteView
-)
+from .views import pages_dropdown, page_detail
 
 urlpatterns = [
-    path("", PageListView.as_view(), name="page_list"),
-    path("crear/", PageCreateView.as_view(), name="page_create"),
-    path("<int:pk>/", PageDetailView.as_view(), name="page_detail"),
-    path("<int:pk>/editar/", PageUpdateView.as_view(), name="page_update"),
-    path("<int:pk>/borrar/", PageDeleteView.as_view(), name="page_delete"),
+    path("", pages_dropdown, name="page_list"),
+    path("<int:pk>/", page_detail, name="page_detail"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,44 +1,12 @@
-from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.contrib.auth.mixins import LoginRequiredMixin
-
-from .models import Page
+from django.shortcuts import render, get_object_or_404
+from web.models import Page  
 
 
-class PageListView(ListView):
-    model = Page
-    template_name = "pages/page_list.html"
-    context_object_name = "pages"
+def pages_dropdown(request):
+    pages = Page.objects.all().order_by("titulo")
+    return render(request, "pages/pages_dropdown.html", {"pages": pages})
 
 
-class PageDetailView(DetailView):
-    model = Page
-    template_name = "pages/pages_detail.html"
-    context_object_name = "page"
-
-
-class PageCreateView(LoginRequiredMixin, CreateView):
-    model = Page
-    fields = ["titulo", "subtitulo", "contenido", "imagen"]
-    template_name = "pages/pages_form.html"
-    success_url = reverse_lazy("page_list")
-
-    def form_valid(self, form):
-        if hasattr(form.instance, "autor_id"):
-            form.instance.autor = self.request.user
-        return super().form_valid(form)
-
-
-class PageUpdateView(LoginRequiredMixin, UpdateView):
-    model = Page
-    fields = ["titulo", "subtitulo", "contenido", "imagen"]
-    template_name = "pages/pages_form.html"
-
-    def get_success_url(self):
-        return reverse_lazy("page_detail", kwargs={"pk": self.object.pk})
-
-
-class PageDeleteView(LoginRequiredMixin, DeleteView):
-    model = Page
-    template_name = "pages/pages_confirm_delete.html"
-    success_url = reverse_lazy("page_list")
+def page_detail(request, pk):
+    page = get_object_or_404(Page, pk=pk)
+    return render(request, "pages/page_detail_simple.html", {"page": page})
